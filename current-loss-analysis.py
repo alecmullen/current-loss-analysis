33e499d4f4245d5ab0df6828b4e444269ed4e19e
Alec Mullen <alec.mullen@berkeley.edu>
2018-05-23 16:24:09 -0700

    Added comments and a couple best fit curves

--- current-loss-analysis.py
+++ current-loss-analysis.py
@@ -3,8 +3,11 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
+''' Class for storing and analyzing QE Data over wavelength (wl). Finds piecwise
+    best fit to estimate current loss.'''
 class QEData():
 
+    #Currently loads data from Excel. TODO: accept args from JMP
     def __init__(self, fname):
         wb = openpyxl.load_workbook(fname, read_only = True, data_only = True)
         data = wb.get_sheet_by_name("Subset of Cell QE All")
@@ -12,13 +15,19 @@ class QEData():
         self.qe = [r[6].value for r in data.rows]
         self.bandGap = [r[21].value for r in data.rows]
 
+    ''' Returns x,y values for a) slopes between QE,WL data points and
+    #   b) slopes between points in a). The idea is to approximate
+    #   first and second derivative values in order to determine partition
+    #   points *before* doing best fits for each partition'''
     def _derivatives(self):
+        # first order derivatives (wl, dqe/dwl)
         slopes = []
         slopes_w = []
         for i, w in enumerate(self.wl[2:96]):
             slopes.append((self.qe[i + 2] - self.qe[i + 1])/(w - self.wl[i + 1]))
             slopes_w.append(w - (w - self.wl[i + 1]) / 2)
 
+        # second order derivatives (wl, d^2qe/dwl^2)
         sec_der = []
         sec_der_w = []
         for i, w in enumerate(slopes_w[1:]):
@@ -27,10 +36,16 @@ class QEData():
 
         return slopes_w, slopes, sec_der_w, sec_der
 
+    ''' Finds best partition points using hard-coded estimates and derivatives.
+        Tries data points for wl values w/in 20nm of estimate for each point.
+        On points predicted to be inflection points, uses 'second derivative'
+        to pick point. On critical points estimated to join two straight lines,
+        uses 'first derivatives'.'''
     def _find_critical_points(self):
         estimates = [410, 520, 580, 950, 1050]
         critical_points = [0, 0, 0, 0, 0]
 
+        # Find points w/in 20nm of estimates
         poss_points = [[], [], [], [], []]
         for i, w in enumerate(self.wl[1:97]):
             for j, e in enumerate(estimates):
@@ -39,30 +54,35 @@ class QEData():
 
         slopes_w, slopes, sec_der_w, sec_der = self._derivatives()
 
+        # Predicted inflection point (threshold 0.001)
         for i, p in enumerate(poss_points[0]):
             j = get_by_w(self.wl[p], sec_der_w, sec_der)
             critical_points[0] = p
             if sec_der[j] > 0.001:
                 break
 
+        #Predicted inflection point (no threshold)
         for i, p in enumerate(poss_points[1]):
             j = get_by_w(self.wl[p], sec_der_w, sec_der)
             critical_points[1] = p
             if sec_der[j] < 0:
                 break
 
+        #Predicted inflection point (threshold 0.002)
         for i, p in enumerate(poss_points[2]):
             j = get_by_w(self.wl[p], sec_der_w, sec_der)
             critical_points[2] = p
             if sec_der[j] > -0.002 and sec_der[j] < 0.002:
                 break
 
+        #Predicted point joining two lines (threshold 0.08)
         for i, p in enumerate(poss_points[3]):
             j = get_by_w(self.wl[p], slopes_w, slopes)
             critical_points[3] = p
             if slopes[j] < -0.08:
                 break
 
+        #Predicted point joining line w/ slope? (threshold 0.18)
         for i, p in enumerate(poss_points[4]):
             j = get_by_w(self.wl[p], slopes_w, slopes)
             critical_points[4] = p
@@ -71,16 +91,27 @@ class QEData():
 
         return critical_points
 
+    ''' Returns piecewise best fit data points'''
     def _best_fits(self):
         critical_points = self._find_critical_points()
+        cp0, cp1, cp2, cp3, cp4 = critical_points
 
-        cp0 = critical_points[0]
-        w = self.wl[1:cp0]
-        qe = self.qe[1:cp0]
+        # First curve: parabola
+        w = self.wl[1:cp0 + 1]
+        qe = self.qe[1:cp0 + 1]
 
-        curve1 = np.poly1d(np.polyfit(w, qe, 3))
-        w_new = np.linspace(w[0], w[cp0 - 2], 50)
-        qe_new = curve1(w_new)
+        curve = np.poly1d(np.polyfit(w, qe, 6))
+        w_new = np.linspace(w[0], w[cp0 - 1], 50)
+        qe_new = curve(w_new)
+
+        # Second curve: parabola
+        w = self.wl[cp0:cp1 + 1]
+        qe = self.qe[cp0:cp1 + 1]
+
+        curve = np.poly1d(np.polyfit(w, qe, 6))
+        w_temp = np.linspace(w[0], w[cp1 - cp0], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
 
         return w_new, qe_new
 
@@ -108,6 +139,7 @@ class QEData():
         plt.grid(True)
         plt.show()
 
+# Returns index for y in y_vals corresponding to wavelength w
 def get_by_w(w, w_vals, y_vals):
     for i, y in enumerate(y_vals):
         if w_vals[i] > w:
