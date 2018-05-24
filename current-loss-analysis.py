8422e41527ddff8525bd6a66aebe940829f94134
Alec Mullen <alec.mullen@berkeley.edu>
2018-05-24 12:44:56 -0700

    Curves done, can call on different device data, incorporating AM15G

--- current-loss-analysis.py
+++ current-loss-analysis.py
@@ -13,19 +13,27 @@ class QEData():
         data = wb.get_sheet_by_name("Subset of Cell QE All")
         self.wl = [r[5].value for r in data.rows]
         self.qe = [r[6].value for r in data.rows]
-        self.bandGap = [r[21].value for r in data.rows]
+        self.device = [r[2].value for r in data.rows]
+        bandGaps = [r[21].value for r in data.rows]
+        self.bandGap = bandGaps[1]
+
+        AM15G = openpyxl.load_workbook("AM1.5G_num_photons.xlsx")
+        AM15G_Data = AM15G.get_sheet_by_name("Data")
+        self.AM_WL = [r[0].value for r in AM15G_Data.rows]
+        self.AM_PHOTONS = [r[1].value for r in AM15G_Data.rows]
 
     ''' Returns x,y values for a) slopes between QE,WL data points and
     #   b) slopes between points in a). The idea is to approximate
     #   first and second derivative values in order to determine partition
     #   points *before* doing best fits for each partition'''
-    def _derivatives(self):
+    def _derivatives(self, device):
+        wl = [w for i, w in enumerate(self.wl) if self.device[i] == device]
         # first order derivatives (wl, dqe/dwl)
         slopes = []
         slopes_w = []
-        for i, w in enumerate(self.wl[2:96]):
-            slopes.append((self.qe[i + 2] - self.qe[i + 1])/(w - self.wl[i + 1]))
-            slopes_w.append(w - (w - self.wl[i + 1]) / 2)
+        for i, w in enumerate(wl[2:96]):
+            slopes.append((self.qe[i + 2] - self.qe[i + 1])/(w - wl[i + 1]))
+            slopes_w.append(w - (w - wl[i + 1]) / 2)
 
         # second order derivatives (wl, d^2qe/dwl^2)
         sec_der = []
@@ -41,111 +49,203 @@ class QEData():
         On points predicted to be inflection points, uses 'second derivative'
         to pick point. On critical points estimated to join two straight lines,
         uses 'first derivatives'.'''
-    def _find_critical_points(self):
+    def _find_critical_points(self, device):
         estimates = [410, 520, 580, 950, 1050]
-        critical_points = [0, 0, 0, 0, 0]
+        critical_points = [0, 0, 0, 0, 0, 0]
+
+        wl = [w for i, w in enumerate(self.wl) if self.device[i] == device]
 
         # Find points w/in 20nm of estimates
         poss_points = [[], [], [], [], []]
-        for i, w in enumerate(self.wl[1:97]):
+        for i, w in enumerate(wl):
             for j, e in enumerate(estimates):
                 if np.abs(w - e) <= 20:
-                    poss_points[j].append(i + 1)
+                    poss_points[j].append(i)
 
-        slopes_w, slopes, sec_der_w, sec_der = self._derivatives()
+        slopes_w, slopes, sec_der_w, sec_der = self._derivatives(device)
 
         # Predicted inflection point (threshold 0.001)
         for i, p in enumerate(poss_points[0]):
-            j = get_by_w(self.wl[p], sec_der_w, sec_der)
+            j = get_by_w(wl[p] + 1, sec_der_w)
             critical_points[0] = p
             if sec_der[j] > 0.001:
                 break
 
-        #Predicted inflection point (no threshold)
+        # Predicted inflection point (no threshold)
         for i, p in enumerate(poss_points[1]):
-            j = get_by_w(self.wl[p], sec_der_w, sec_der)
+            j = get_by_w(wl[p] + 1, sec_der_w)
             critical_points[1] = p
             if sec_der[j] < 0:
                 break
 
-        #Predicted inflection point (threshold 0.002)
+        # Predicted inflection point (threshold 0.002)
         for i, p in enumerate(poss_points[2]):
-            j = get_by_w(self.wl[p], sec_der_w, sec_der)
+            j = get_by_w(wl[p] + 1, sec_der_w)
             critical_points[2] = p
-            if sec_der[j] > -0.002 and sec_der[j] < 0.002:
+            if sec_der[j] > -0.001 and sec_der[j] < 0.001:
                 break
 
-        #Predicted point joining two lines (threshold 0.08)
+        # Predicted point joining two lines (threshold 0.08)
         for i, p in enumerate(poss_points[3]):
-            j = get_by_w(self.wl[p], slopes_w, slopes)
+            j = get_by_w(wl[p] + 1, slopes_w)
             critical_points[3] = p
             if slopes[j] < -0.08:
                 break
 
-        #Predicted point joining line w/ slope? (threshold 0.18)
+        # Predicted point joining line w/ curve? (threshold 0.18)
         for i, p in enumerate(poss_points[4]):
-            j = get_by_w(self.wl[p], slopes_w, slopes)
+            j = get_by_w(wl[p] + 1, slopes_w)
             critical_points[4] = p
             if slopes[j] < -0.18:
                 break
 
+        #Last critical point is bandGap
+        critical_points[5] = get_by_w(1241.52/self.bandGap, wl)
+
         return critical_points
 
     ''' Returns piecewise best fit data points'''
-    def _best_fits(self):
-        critical_points = self._find_critical_points()
-        cp0, cp1, cp2, cp3, cp4 = critical_points
+    def _best_fits(self, device):
+        critical_points = self._find_critical_points(device)
+        cp0, cp1, cp2, cp3, cp4, bandGap_i = critical_points
+
+        wl = [w for i, w in enumerate(self.wl) if self.device[i] == device]
+        qe = [q for i, q in enumerate(self.qe) if self.device[i] == device]
+
+        w_new = self.AM_WL[1:]
 
         # First curve: parabola
-        w = self.wl[1:cp0 + 1]
-        qe = self.qe[1:cp0 + 1]
+        w = wl[:cp0]
+        q = qe[:cp0]
 
-        curve = np.poly1d(np.polyfit(w, qe, 6))
+        curve = np.poly1d(np.polyfit(w, q, 3))
         w_new = np.linspace(w[0], w[cp0 - 1], 50)
         qe_new = curve(w_new)
 
         # Second curve: parabola
-        w = self.wl[cp0:cp1 + 1]
-        qe = self.qe[cp0:cp1 + 1]
+        w = wl[cp0 - 1:cp1]
+        q = qe[cp0 - 1:cp1]
 
-        curve = np.poly1d(np.polyfit(w, qe, 6))
+        curve = np.poly1d(np.polyfit(w, q, 2))
         w_temp = np.linspace(w[0], w[cp1 - cp0], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
+
+        # Third curve: parabola
+        w = wl[cp1 - 1:cp2 - 1]
+        q = qe[cp1 - 1:cp2 - 1]
+
+        curve = np.poly1d(np.polyfit(w, q, 2))
+        w_temp = np.linspace(w[0], w[cp2 - cp1 - 1], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
+
+        # Fourth curve: line
+        w = wl[cp2 - 1:cp3]
+        q = qe[cp2 - 1:cp3]
+
+        curve = np.poly1d(np.polyfit(w, q, 2))
+        w_temp = np.linspace(w[0], w[cp3 - cp2], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
+
+        # Fifth curve: line
+        w = wl[cp3 - 1:cp4]
+        q = qe[cp3 - 1:cp4]
+
+        curve = np.poly1d(np.polyfit(w, q, 1))
+        w_temp = np.linspace(w[0], w[cp4 - cp3], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
+
+        # Sixth curve: parabola. End at bandGap cutoff
+        w = wl[cp4 - 1:bandGap_i]
+        q = qe[cp4 - 1:bandGap_i]
+
+        curve = np.poly1d(np.polyfit(w, q, 2))
+        w_temp = np.linspace(w[0], w[bandGap_i - cp4], 50)
+        w_new = np.append(w_new, w_temp)
+        qe_new = np.append(qe_new, curve(w_temp))
+
+        # Sixth curve: parabola. bandGap cutoff to end
+        end_i = get_by_w(1300, wl)
+
+        w = wl[bandGap_i - 1:end_i + 1]
+        q = qe[bandGap_i - 1:end_i + 1]
+
+        curve = np.poly1d(np.polyfit(w, q, 3))
+        w_temp = np.linspace(w[0], w[end_i - bandGap_i + 1], 50)
         w_new = np.append(w_new, w_temp)
         qe_new = np.append(qe_new, curve(w_temp))
 
         return w_new, qe_new
 
-    def plot(self):
-        #slopes_w, slopes, sec_der_w, sec_der = self._derivatives()
-        critical_points_w = [self.wl[cp] for cp in self._find_critical_points()]
-        critical_points_qe = [self.qe[cp] for cp in self._find_critical_points()]
+    def integral(self, device, a, b):
+        w, q = self._best_fits(device)
+        i, j = get_by_w(a, w), get_by_w(b, w)
+        return np.sum(q[i:j]) * (b - a) / len(q[i:j])
 
-        best_fit_x, best_fit_y = self._best_fits()
+    def overallQE(self, device):
+        return self.integral(device, 360, 1300) / ((1300 - 360) * 100)
 
-        #plt.subplot(311)
-        QEcurve = plt.plot(self.wl[1:96], self.qe[1:96], '-o',
+    def plot(self, device):
+        wl = [w for i, w in enumerate(self.wl) if self.device[i] == device]
+        qe = [q for i, q in enumerate(self.qe) if self.device[i] == device]
+        if (wl == []):
+            raise Exception('No such device ID found in spreadsheet.')
+
+        critical_points_w = [wl[cp - 1] for cp in self._find_critical_points(device)]
+        critical_points_qe = [qe[cp - 1] for cp in self._find_critical_points(device)]
+
+        best_fit_x, best_fit_y = self._best_fits(device)
+
+        QEcurve = plt.plot(wl, qe, '-o',
             critical_points_w, critical_points_qe, 'ro',
             best_fit_x, best_fit_y)
         plt.axis([300, 1400, 0, 100])
 
-        '''plt.subplot(312)
+    def plot_derivatives(self, device):
+        wl = [w for i, w in enumerate(self.wl) if self.device[i] == device]
+        qe = [q for i, q in enumerate(self.qe) if self.device[i] == device]
+
+        slopes_w, slopes, sec_der_w, sec_der = self._derivatives(device)
+
+        plt.subplot(311)
+        QEcurve = plt.plot(wl, qe, '-o')
+        plt.axis([300, 1400, 0, 100])
+
+        plt.subplot(312)
         slopeCurve = plt.plot(slopes_w, slopes, '-o')
         plt.axis([300, 1400, -0.8, 0.8])
 
         plt.subplot(313)
         secDerCurve = plt.plot(sec_der_w, sec_der, '-o')
-        plt.axis([300, 1400, -0.025, 0.025])'''
+        plt.axis([300, 1400, -0.025, 0.025])
 
-        plt.grid(True)
-        plt.show()
 
-# Returns index for y in y_vals corresponding to wavelength w
-def get_by_w(w, w_vals, y_vals):
-    for i, y in enumerate(y_vals):
-        if w_vals[i] > w:
+# Returns index corresponding to wavelength w
+def get_by_w(w, w_vals):
+    for i, v in enumerate(w_vals):
+        if v >= w:
             return i
     return -1
 
 if __name__ == "__main__":
     QEData = QEData(os.path.join(os.pardir, 'current-loss-analysis/QEData.xlsx'))
-    QEData.plot()
+
+    print("Overall QE:", QEData.overallQE(device = 200000))
+
+    plt.figure(1)
+    QEData.plot(200000)
+    plt.grid(True)
+
+    '''plt.figure(2)
+    QEData.plot('2G5')
+    plt.grid(True)
+
+    plt.figure(3)
+    QEData.plot('2K5')
+    plt.grid(True)'''
+
+    #QEData.plot_derivatives(200000)
+    plt.show()
