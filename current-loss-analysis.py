a14098ab12a16d57387a1d359ea56cdc0bc11aae
Alec Mullen <alec.mullen@berkeley.edu>
2018-06-04 16:20:34 -0700

    Python only takes in data from one device id, doesn't have to filter. JSL takes device id instead of datetime:

--- current-loss-analysis.py
+++ current-loss-analysis.py
@@ -9,12 +9,10 @@ JMP = True
 
 #JMP Scripts sends data + path for AM15G Excel data
 if JMP:
-    jmp_datetime = eval(input("DateTime: "))
     jmp_wl = eval(input("WL: "))
     jmp_qe = eval(input("QE: "))
     jmp_bandgap = eval(input("Bandgap: "))
     AM15DATAPATH = input("AM1.5G Data Path: ")
-    this_datetime = input("Enter datetime: ")
 else:
     AM15DATAPATH = 'AM1.5G.xlsx'
 
@@ -26,42 +24,33 @@ class QEData():
     def __init__(self, fname = ''):
         if JMP:
             self.wl = jmp_wl
-            self.wl.insert(0, 'WL')
             self.qe = jmp_qe
-            self.qe.insert(0, 'QE')
-            try:
-                self.datetime = [datetime.datetime.strptime(d, '%m/%d/%Y %I:%M:%S %p' ) for d in jmp_datetime]
-            except Exception as e:
-                raise Exception('Error in datetime format')
-            self.datetime.insert(0, 'DateTime')
             self.bandGap = jmp_bandgap[0]
         else:
             wb = openpyxl.load_workbook(fname, read_only = True, data_only = True)
             data = wb.get_sheet_by_name('Subset of Cell QE All')
-            self.wl = [r[5].value for r in data.rows]
-            self.qe = [r[6].value for r in data.rows]
-            self.datetime = [r[0].value for r in data.rows]
-            bandGaps = [r[21].value for r in data.rows]
+            self.wl = [r[5].value for r in data.rows][1:]
+            self.qe = [r[6].value for r in data.rows][1:]
+            self.datetime = [r[0].value for r in data.rows][1:]
+            bandGaps = [r[21].value for r in data.rows][1:]
             self.bandGap = bandGaps[1]
 
         AM15G = openpyxl.load_workbook(AM15DATAPATH)
         AM15G_Data = AM15G.get_sheet_by_name('Data')
-        self.AM_WL = [r[0].value for r in AM15G_Data.rows]
-        self.AM_PHOTONS = [r[1].value for r in AM15G_Data.rows]
+        self.AM_WL = [r[0].value for r in AM15G_Data.rows][1:]
+        self.AM_PHOTONS = [r[1].value for r in AM15G_Data.rows][1:]
 
         #Fixes points from bad filter by shifting up to make 'continuous'
-        self.bad_points = {datetime: True for datetime in self.datetime}
-        for d in self.bad_points.keys():
-            if d != 'DateTime':
-                self._fix_points(d)
+        self.bad_points = True
+        self._fix_points()
 
     ''' Returns x,y values for a) slopes between QE,WL data points and
     #   b) slopes between points in a). The idea is to approximate
     #   first and second derivative values in order to determine partition
     #   points *before* doing best fits for each partition'''
-    def _derivatives(self, datetime):
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-        qe = [q for i, q in enumerate(self.qe) if self.datetime[i] == datetime]
+    def _derivatives(self):
+        wl = self.wl
+        qe = self.qe
         # first order derivatives (wl, dqe/dwl)
         slopes = []
         slopes_w = []
@@ -83,18 +72,11 @@ class QEData():
         On points predicted to be inflection points, uses 'second derivative'
         to pick point. On critical points estimated to join two straight lines,
         uses 'first derivatives'.'''
-    def _find_critical_points(self, datetime):
+    def _find_critical_points(self):
+        wl = self.wl
+        qe = self.qe
         estimates = [420, 520, 580, 950, 1050]
         critical_points = [0, 0, 0, 0, 0, 0]
-
-        datetime_i = 0
-        wl = []
-        for i, w in enumerate(self.wl):
-            if self.datetime[i] == datetime:
-                if datetime_i == 0:
-                    datetime_i = i
-                wl += [w]
-        qe = [q for i, q in enumerate(self.qe) if self.datetime[i] == datetime]
 
         # Find points w/in 20nm of estimates
         poss_points = [[], [], [], [], []]
@@ -103,7 +85,7 @@ class QEData():
                 if np.abs(w - e) <= 20:
                     poss_points[j].append(i)
 
-        slopes_w, slopes, sec_der_w, sec_der = self._derivatives(datetime)
+        slopes_w, slopes, sec_der_w, sec_der = self._derivatives()
 
         # Predicted inflection point (threshold 0.001)
         for i, p in enumerate(poss_points[0]):
@@ -150,31 +132,29 @@ class QEData():
 
     # Fixes points before third critical point by shifting up to make 'continuous'.
     # Accounts for bad filter on machine
-    def _fix_points(self, datetime):
-        if self.bad_points[datetime]:
-            critical_points = self._find_critical_points(datetime)
-            wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-            datetime_i = self.datetime.index(datetime)
+    def _fix_points(self):
+        if self.bad_points:
+            critical_points = self._find_critical_points()
+            wl = self.wl
 
-            jump_point = critical_points[2]
-            jp = get_by_w(wl[jump_point], self.wl[datetime_i:]) + datetime_i
+            jp = critical_points[2]
             jump = self.qe[jp] - self.qe[jp - 1]
             jump -= self.qe[jp - 1] - self.qe[jp - 2]
             jump += 2 * self.qe[jp - 2] - self.qe[jp - 3] - self.qe[jp - 1]
 
-            for i, q_val in enumerate(self.qe[datetime_i: jp]):
-                self.qe[datetime_i + i] = q_val + jump
-            self.bad_points[datetime] = False
+            for i, q_val in enumerate(self.qe[0: jp]):
+                self.qe[i] = q_val + jump
+            self.bad_points = False
 
     ''' Returns piecewise best fit data points'''
-    def _best_fits(self, datetime):
-        critical_points = self._find_critical_points(datetime)
+    def _best_fits(self):
+        critical_points = self._find_critical_points()
         cp0, cp1, cp2, cp3, cp4, bandGap_i = critical_points
 
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-        qe = [q for i, q in enumerate(self.qe) if self.datetime[i] == datetime]
+        wl = self.wl
+        qe = self.qe
 
-        w_new = self.AM_WL[1:]
+        w_new = self.AM_WL
 
         # First curve: parabola
         w = wl[:cp0 + 2]
@@ -256,63 +236,60 @@ class QEData():
         return w_new, qe_new.tolist()
 
     # Takes integral of QE (not used)
-    def integralQE(self, datetime):
-        w, q = self._best_fits(datetime)
+    def integralQE(self):
+        w, q = self._best_fits()
         return integral(w, q) / ((1300 - 360) * 100)
 
     # Calculates total current loss by using best fit, convolving w/ Am15G data,
     # taking integral and subtracting from Am1.5G curve
-    def current_loss(self, datetime):
-        w, q = self._best_fits(datetime)
-        AM15G_convolution = [q[i] / 100 * p for i, p in enumerate(self.AM_PHOTONS[1:])]
+    def current_loss(self):
+        w, q = self._best_fits()
+        AM15G_convolution = [q[i] / 100 * p for i, p in enumerate(self.AM_PHOTONS)]
         plt.plot(w, AM15G_convolution)
 
         current = integral(w, AM15G_convolution) / COULOMBS
-        return (integral(w, self.AM_PHOTONS[1:]) / COULOMBS) - current
+        return (integral(w, self.AM_PHOTONS / COULOMBS) - current)
 
     # See above, but also returns best fit data, convolution, current generated,
     # and current data for partitions
-    def current_loss_all_data(self, datetime):
-        critical_points = self._find_critical_points(datetime)
-        w, q = self._best_fits(datetime)
-        AM15G_convolution = [q[i] / 100 * p for i, p in enumerate(self.AM_PHOTONS[1:])]
+    def current_loss_all_data(self):
+        critical_points = self._find_critical_points()
+        w, q = self._best_fits()
+        AM15G_convolution = [q[i] / 100 * p for i, p in enumerate(self.AM_PHOTONS)]
         #plt.plot(w, AM15G_convolution)
         current_partitions_loss = [0, 0, 0, 0, 0, 0, 0]
         current_partitions = [0, 0, 0, 0, 0, 0, 0]
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
+        wl = self.wl
 
         j = 360
         crit_ps = [wl[cp] for cp in critical_points]
         for i in range(6):
             crit_p = crit_ps[i]
             curr = integral(w, AM15G_convolution, a=j, b=crit_p) / COULOMBS
-            curr_loss = (integral(w, self.AM_PHOTONS[1:], a=j, b=crit_p) / COULOMBS ) - curr
+            curr_loss = (integral(w, self.AM_PHOTONS, a=j, b=crit_p) / COULOMBS ) - curr
             current_partitions[i] = curr
             current_partitions_loss[i] = curr_loss
             j = crit_p
 
         curr = integral(w, AM15G_convolution, a=j) / COULOMBS
-        curr_loss = (integral(w, self.AM_PHOTONS[1:], a = j) / COULOMBS) - curr
+        curr_loss = (integral(w, self.AM_PHOTONS, a = j) / COULOMBS) - curr
         current_partitions[6] = curr
         current_partitions_loss[6] = curr_loss
 
         current = integral(w, AM15G_convolution) / COULOMBS
-        current_loss = (integral(w, self.AM_PHOTONS[1:]) / COULOMBS) - current
+        current_loss = (integral(w, self.AM_PHOTONS) / COULOMBS) - current
         return (w, q, AM15G_convolution, current_loss, current, crit_ps,
                 current_partitions_loss, current_partitions)
 
     #Plots critical points, best fit, and original data (w/ fixed points)
-    def plot(self, datetime):
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-        if (wl == []):
-            raise Exception('No such datetime ID found in spreadsheet.')
+    def plot(self):
+        wl = self.wl
+        qe = self.qe
 
-        best_fit_x, best_fit_y = self._best_fits(datetime)
+        best_fit_x, best_fit_y = self._best_fits()
 
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-        qe = [q for i, q in enumerate(self.qe) if self.datetime[i] == datetime]
-        critical_points_w = [wl[cp] for cp in self._find_critical_points(datetime)]
-        critical_points_qe = [qe[cp] for cp in self._find_critical_points(datetime)]
+        critical_points_w = [wl[cp] for cp in self._find_critical_points()]
+        critical_points_qe = [qe[cp] for cp in self._find_critical_points()]
 
         QEcurve = plt.plot(wl, qe, '-o',
             critical_points_w, critical_points_qe, 'ro',
@@ -329,13 +306,11 @@ class QEData():
         plt.plot(slopes_w, slopes)
 
     # Plots qe data, first and decond 'derivatives'
-    def plot_derivatives(self, datetime):
-        wl = [w for i, w in enumerate(self.wl) if self.datetime[i] == datetime]
-        qe = [q for i, q in enumerate(self.qe) if self.datetime[i] == datetime]
-        if (wl == []):
-            raise Exception('No such datetime ID found in spreadsheet.')
+    def plot_derivatives(self):
+        wl = self.wl
+        qe = self.qe
 
-        slopes_w, slopes, sec_der_w, sec_der = self._derivatives(datetime)
+        slopes_w, slopes, sec_der_w, sec_der = self._derivatives()
 
         plt.subplot(311)
         QEcurve = plt.plot(wl, qe, '-o')
@@ -372,14 +347,13 @@ if __name__ == '__main__':
     # Talks to JMP
     if (JMP):
         QEData = QEData()
-        time = datetime.datetime.strptime(this_datetime, '%m/%d/%Y %I:%M:%S %p' )
         w, q, convolution, current_loss, current, critical_points, \
-            current_partitions_loss, current_partitions = QEData.current_loss_all_data(time)
+            current_partitions_loss, current_partitions = QEData.current_loss_all_data()
 
         print('Wavelengths:', w)
         print('Best Fits:', q)
         print('Convolution:', convolution)
-        print('AM15G:', QEData.AM_PHOTONS[1:])
+        print('AM15G:', QEData.AM_PHOTONS)
         print('Current Loss:', current_loss, 'A/m^2')
         print('Current Generated:', current, 'A/m^2')
         print('Critical Points:', critical_points)
@@ -390,16 +364,15 @@ if __name__ == '__main__':
         #testing stuff
         QEData = QEData(os.path.join(os.pardir, 'current-loss-analysis/QEData.xlsx'))
 
-        first_time = datetime.datetime(2018, 5, 15, 20, 28, 0)
 
-        print('Integral QE over WL:', QEData.integralQE(datetime = first_time))
+        print('Integral QE over WL:', QEData.integralQE())
 
         plt.figure(2)
-        QEData.plot(first_time)
+        QEData.plot()
         plt.grid(True)
 
         plt.figure(3)
-        print('Current Loss:', QEData.current_loss(first_time), 'A/m^2')
+        print('Current Loss:', QEData.current_loss(), 'A/m^2')
 
-        QEData.plot_derivatives(first_time)
+        QEData.plot_derivatives()
         plt.show()
